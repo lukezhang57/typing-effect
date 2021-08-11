10675da5780a9aae295ed7f38ba1f05bfb7c3f49
Luke Zhang <lukezhangxc@gmail.com>
2021-08-10 19:17:46 -0500

    added a backspace effect

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-const words = ['run','bike','code','explore','seehowlongyoucanreadthiswithouttakingyoureyesoff']
+const words = ['run','bike','code','explore','create']
 let listindex = 0;
 let wordindex = 0;
 let letter = '';
+let atEnd = false;
 
 function type(){
-    if (listindex == words.length){
-        listindex = 0;
+    if (!atEnd){
+        if (listindex == words.length){
+            listindex = 0;
+        }
+        if (wordindex < words[listindex].length){
+            letter = words[listindex].slice(0,++wordindex);
+            document.querySelector('.word').textContent = letter;
+        }
+        else if (wordindex === words[listindex].length){
+            letter = letter + ".";
+            document.querySelector('.word').textContent = letter;
+            wordindex++;
+        }
+        else if (wordindex > words[listindex].length){
+            atEnd = true;
+        }
     }
-    letter = " " + words[listindex].slice(0,++wordindex);
-    document.querySelector('.word').textContent = letter;
-    if (wordindex === words[listindex].length){
-        letter = letter + ".";
+    else {
+        if (wordindex > words[listindex].length){ 
+            letter = words[listindex].slice(0,words[listindex].length);
+            wordindex--;
+        }
+        letter = words[listindex].slice(0,wordindex);
         document.querySelector('.word').textContent = letter;
-        listindex++;
-        wordindex = 0;
+        wordindex--;
+        if (wordindex < 0){
+            atEnd = false;
+            listindex++;
+            wordindex = 0;
+        }
     }
-    setTimeout(type,300)    
+    setTimeout(type,200) 
 }; 
 
-type(); 
+type();
